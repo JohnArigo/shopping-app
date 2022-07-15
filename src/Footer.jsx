941477d8c53c54c841485334e0ad06db206df0e8
RaggedyRagz <JohnArigo96706@gmail.com>
2022-07-14 18:36:35 -0700

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css"
+
+export default function Footer(){
+
+    return(
+        <footer>
+            <section className="footer-social">
+                <div className="social-app">Social Links Here</div>
+                <div className="social-app">Social Links Here</div>
+                <div className="social-app">Social Links Here</div>
+                <div className="social-app">Social Links Here</div>
+            </section>
+            <section className="footer-copyright">
+                <div className="copyright">email here</div>
+                <div className="copyright">copyright info here</div>
+            </section>
+        </footer>
+    )
+}
---
+++ src/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css"
+import cartIcon from "./shopping-cart.png"
+
+export default function NavBar({cart}){
+    return (
+        <nav>
+            <section>E-Commerence Project</section>
+            <section><img src={cartIcon} /><label className="cart-label">{cart.length}</label></section>
+        </nav>
+    )
+}
