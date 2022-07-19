65a5f423e581fe44d1d6226273a56bf1fa298ab5
RaggedyRagz <JohnArigo96706@gmail.com>
2022-07-18 22:15:20 -0700

    edited cart array into object

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,27 @@
 import './NavBar.css'
 import cartIcon from './shopping-cart.png'
 
+const getTotalCartCount = (cart) => {
+    let total = 0
+    const cartKeysArray = Object.keys(cart)
+
+    cartKeysArray.forEeach((itmId) => {
+        const item = cart[itemId]
+        const itemCount = item.count
+
+        total += itemCount
+    })
+
+    return total
+}
+
 export default function NavBar({ cart, setMainDisplay }) {
     return (
         <nav>
             <section>E-Commerence Project</section>
             <section>
                 <img onClick={() => setMainDisplay(true)} src={cartIcon} />
-                <label className="cart-label">{cart.length}</label>
+                <label className="cart-label">{getTotalCartCount(cart)}</label>
             </section>
         </nav>
     )
